b1b335fef07e1892a8aa8a671358848e8b9ce5f7
Colin Rho <colinhwrho@gmail.com>
2015-08-12 14:08:33 +0900

    8.12

--- EDA.R
+++ EDA.R
@@ -1,8 +1,20 @@
 rm(list=ls())
+
+## 파일 읽기, 데이터 정리 
 filelist <- list.files(path=getwd(), pattern=".csv")
 filename <- substr(filelist, 1, nchar(filelist)-4)
-DB <- lapply(filelist, read.csv, header=T)
-
+DB <- lapply(filelist, read.csv, header=T)  # 통합된 데이터, list
 for ( i in 1:length(filename)) {
-  assign(filename[i], DB[[i]])
+  assign(filename[i], DB[[i]])  # 각 파일명 matrix로 할당
 }
+
+yearly <- function( x, year ) {
+  ret <- list()
+  for ( i in 1:length(x) ) {
+    ret[[i]] <- subset(x[[i]], YEAR == year)
+  }
+  return (ret)
+}
+
+yearly(DB, 2014)  
+
