e149a369534bd382d0c6feb0bd11c509c94d900c
Colin Rho <colinhwrho@gmail.com>
2015-08-13 17:31:45 +0900

    temporary sheet

--- EDA.R
+++ EDA.R
@@ -1,12 +1,5 @@
 rm(list=ls())
 
-## 파일 읽기, 데이터 정리 
-filelist <- list.files(path=getwd(), pattern=".csv")
-filename <- substr(filelist, 1, nchar(filelist)-4)
-DB <- lapply(filelist, read.csv, header=T)  # 통합된 데이터, list
-for ( i in 1:length(filename)) {
-  assign(filename[i], DB[[i]])  # 각 파일명 matrix로 할당
-}
 
 ## 연도별 데이터 추출 함수
 yearly <- function( x, year ) { 
