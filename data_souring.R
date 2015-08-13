91a14d8c612a0669a0d71c8ec48e8eabff9bac80
Colin Rho <colinhwrho@gmail.com>
2015-08-13 17:31:19 +0900

    file name and structure change

--- data_souring.R
+++ data_souring.R
@@ -1,7 +1,14 @@
 library(XML)
-source("crawl.R")
+source("crawl_functions.R")
+## csv파일 읽기, 데이터 정리 
+filelist <- list.files(path=getwd(), pattern=".csv")
+filename <- substr(filelist, 1, nchar(filelist)-4)
+DB <- lapply(filelist, read.csv, header=T)  # 통합된 데이터, list
+for ( i in 1:length(filename)) {
+  assign(filename[i], DB[[i]])  # 각 파일명 matrix로 할당
+}
 
-## 루프
+## crawling 루프
 for( i in 1:nrow(player_id) ) {
   vec <- player_id[i,] 
   name <- as.character(vec$name)
