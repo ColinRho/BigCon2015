d3d6ddc8c0db5594ec3c883f7c093c5ba4d1d593
Colin Rho <colinhwrho@gmail.com>
2015-09-04 21:21:36 +0900

    data_sourcing on work

--- data_souring.R
+++ data_souring.R
@@ -2,14 +2,20 @@ library(XML)
 library(rvest)
 library(plyr)
 library(data.table)
+library(ggplot2)
+library(reshape2)
+library(randomForest)
+library(cvTools)
 
 ## 함수 실행에 필요한 데이터 파일들: gamelist, lineup, samename, player_id
 
 ## 함수 파일 불러오기
 source("crawl_part.R",encoding="UTF-8")
 source("match_playerdata.R",encoding="UTF-8")
+source("modeling.R")
 
-## 1. csv로 있는 파일 읽기
+########## 1. reading csv files in the working directory ###################################
+
 filelist <- list.files(path=getwd(), pattern=".csv")
 filename <- substr(filelist, 1, nchar(filelist)-4)
 DB <- lapply(filelist, read.csv, header=T)  # 통합된 데이터, list
@@ -17,7 +23,9 @@ for ( i in 1:length(filename)) {
   assign(filename[i], DB[[i]])  # 각 파일명 matrix로 할당
 }
 
-## 2. 선수목록에서 동명이인 이름 바꾸기, 리스트 출력
+############################################################################################
+########## 2. some modifications in player list and traded player list #####################
+
 samename <- homonym(player_id)$samename # 동명이인 리스트
 player_id <- homonym(player_id)$dat # 수정된 선수목록
 
@@ -25,8 +33,9 @@ player_id <- homonym(player_id)$dat # 수정된 선수목록
 trade_2015$date <- as.Date(trade_2015$date)
 trade_2015$name <- apply( trade_2015[,c(2,5)], 1, function(x) change.homonym( x[1], x[2]) )
 
+############################################################################################
+########## 3. personal stats in 2014  ######################################################
 
-## 3. 2014년도 개인 데이터
 a <- subset( pitcher_select, select=c(name, team) )
 pitcher_select$name <- apply( a, 1, function(x) change.homonym(x[1], x[2] ) )
 rm(a)
@@ -34,18 +43,54 @@ a <- subset( hitter_select, select=c(name, team) )
 hitter_select$name <- apply( a, 1, function(x) change.homonym(x[1], x[2] ) )
 rm(a)
 
-  ## 투수는 각 지표들을 이닝수로 나누었고, 타자들은 타수로 나누었다. 
-  pitcher_2014 <- subset(pitcher_select, year == 2014)
-  pitcher_2014 <- pitcher_2014[ , ! colnames(pitcher_2014) %in% c("year") ]
-  hitter_2014 <- subset(hitter_select, year == 2014)
-  hitter_2014 <- hitter_2014[ , ! colnames(hitter_2014) %in% c("year") ]
+pitcher_2014 <- subset(pitcher_select, year == 2014)
+pitcher_2014 <- pitcher_2014[ , ! colnames(pitcher_2014) %in% c("year") ]
+hitter_2014 <- subset(hitter_select, year == 2014)
+hitter_2014 <- hitter_2014[ , ! colnames(hitter_2014) %in% c("year") ]
   
-## 3. crawling 루프(선수 개인별 이번 시즌 데이터)
+########## 3. crawling personal daily data of all players in the list ######################
+### CAUTION: 
+
 crawl.loop ( file = player_id, write.as.csv=F)
-  
-## 4. 2015년 모든 경기들 목록
+
+############################################################################################
+########## 4. list of all games in 2015 season #############################################
+
 month <- c("03","04","05","06","07","08","09")
-gamelist <- gamelist.total ( month, year = "2015" ) # 경기목록
+gamelist <- gamelist.total ( month, year = "2015" )
+games2015 <- subset ( gamelist2015, !is.na(score) ) # without cancelled games
+
+############################################################################################
+
+
+
+
+
+## 5. Regression Approach
+
+##############################################################################
+
+
+
+## real win rates
+real2015 <- rate_func( games = games2015, rate = "real" )
+
+## pythagorean win rates
+pyth2015 <- rate_func( games = games2015, rate = "pyth", power = 2) # with typical power index
+
+## plot examples
+rate_plot(real2015, since = "2015-07-30") + stat_smooth(method = 'lm')
+comp_plot( "LG", rate2015, pyth2015, since="2015-05-01"  )
+
+
+## power index variation
+pyth0.9 <- rate_func( games2015, rate = "pyth", power = 0.9)
+pyth1 <- rate_func( games2015, rate = "pyth", power = 1)
+pyth3 <- rate_func( games2015, rate = "pyth", power = 3)
+pyth2.3 <- rate_func( games2015, rate = "pyth", power = 2.3)
+comp_plot("넥센", rate2015, pyth1, since = "2015-08-01")
+
+### https://en.wikipedia.org/wiki/Pythagorean_expectation
 
 ## 5. 경기목록의 모든 경기의 라인업
 #### 주의: 매 경기마다 페이지를 크롤링해오기 때문에 시간이 꽤 소요됨(네트워크환경이 좋은상태를 권장)
